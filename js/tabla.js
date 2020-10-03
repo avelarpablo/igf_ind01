387bc7f8986e4fe2ecdb18a039afde2fe72cab60
Pablo Avelar <am16015@ues.edu.sv>
2020-10-03 14:00:47 -0600

    Funcionalidad nuevo registro agregada a Tabla

--- js/tabla.js
+++ js/tabla.js
@@ -8,4 +8,9 @@ $(document).ready(function(){
     $('.btn-eliminar').on('click', function(){
         $(this).closest('tr').hide();
     });
+
+    // Acción para botón nuevo registro
+    $('#btnNuevoRegistro').on('click', function(){
+        location.href = $(this).attr('data-url-formulario');
+    });
 });
